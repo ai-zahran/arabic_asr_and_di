d2e5152312cc37dce13c88daf7e75e4ef4c16dd6
Ahmed Ismail <33787886+ai-zahran@users.noreply.github.com>
2019-07-27 22:23:12 +0200

    Remove path_custom.sh functionality from path.sh

--- arabic_asr/path.sh
+++ arabic_asr/path.sh
@@ -1,29 +1,3 @@
-#!/bin/bash
-#: Title : path.sh
-#: Author : "Ahmed Ismail" <ahmed.ismail.zahran@gmail.com>
-#: Version : 1.0
-#: Description : Defines paths to programs used.
-
-# This scripts defines paths to programs used.
-# If a path does not exist, the script reports an error
-
-# Read program paths from paths.txt
-paths_file="paths.txt"
-echo "Reading paths to program directories"
-while IFS=': ', read -r program_name program_path; do
-    if [ "$line" != "" ]; then
-        continue
-    elif  [ ${program_name,,} = 'varikn' ]; then
-        export VariKN_bin=$program_path
-        echo "VariKN path set to $program_path"
-    fi
-done < $paths_file
-
-
-if [ ! -d $VariKN_bin ]; then
-    echo "VariKN binary directory not found in the path specified: $VariKN_bin"
-fi
-
 export KALDI_ROOT=`pwd`/../../..
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
